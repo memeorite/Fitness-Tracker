a5a5a583a622bcb34acb2edfa1e534a5e52826bc
mandybrandt <mandybrandt2001@yahoo.com>
2022-10-16 15:30:23 -0600

    users api complete, but not passing

--- FitnessTracker/api/routines.js
+++ FitnessTracker/api/routines.js
@@ -1,10 +1,18 @@
+const { application } = require('express');
 const express = require('express');
 const router = express.Router();
 
+const { getRoutines, getRoutinesById, getAllPublicRoutines } = require('../db')
+
 // GET /api/routines
-
+router.get('./routines',(req, res)=> {
+    res.send(getAllPublicRoutines)
+})
 // POST /api/routines
+router.post('./routines', async (req,res,next)=> {
 
+
+})
 // PATCH /api/routines/:routineId
 
 // DELETE /api/routines/:routineId
